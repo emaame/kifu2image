4edab577a1e1512e1dc8614eeb2818dd08b9c671
ema <ameema@gmail.com>
2017-05-03 21:01:36 +0900

    init.

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+var path = require('path');
+
+module.exports = [{
+    entry: ['./src/app.js'],
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'public')
+    },
+    module: {
+        rules: [{
+            test: /\.(css|sass|scss)$/,
+            use: [
+                'style-loader',
+                'css-loader',
+                'sass-loader'
+            ]
+        }]
+    }
+}];
+
